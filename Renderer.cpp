46df1efd13597328399660ad01e2d4db644855c3
Kosmokleaner <Kosmokleaner@Kosmokleaner.de>
2009-01-04 20:47:21 +0100

    Signed-off-by: Kosmokleaner <Kosmokleaner@Kosmokleaner.de>

--- Renderer.cpp
+++ Renderer.cpp
@@ -11,8 +11,8 @@
 
 
 
-//CRenderer g_Renderer(640,480);
-CRenderer g_Renderer(640/4,480/4);
+CRenderer g_Renderer(640,480);
+//CRenderer g_Renderer(640/4,480/4);
 //CRenderer g_Renderer(640/2,480/2);
 //CRenderer g_Renderer(128,128);
 
